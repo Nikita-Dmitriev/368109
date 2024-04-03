133a6598f5aadc37e1b1369435bce9f2d73bb750
Nikita-Dmitriev <dmitriev_nikita228@mail.ru>
2024-04-03 15:45:29 +0300

    lab2

--- src/lab2/caesar.py
+++ src/lab2/caesar.py
@@ -1,6 +1,7 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -11,13 +12,21 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for char in plaintext:
+        if char.isalpha():  # идем дальше только если символ является буквой
+            if char.isupper():  # условие, чтобы в дальнейшем не выпасть из заглавных или строчных букв
+                start_position = ord('A')
+            else:
+                start_position = ord('a')
+            ciphertext += chr((ord(char) + shift - start_position) % 26 + start_position) # сдвигаем символ на указаный shift и добавляем в вывод
+        else:
+            ciphertext += char  # если символ не буква, оставляем как есть
     return ciphertext
-
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +37,13 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for char in ciphertext:
+        if char.isalpha():
+            if char.isupper():  # проверка на базу шифрования
+                start_position = ord('A')
+            else:
+                start_position = ord('a')
+            plaintext += chr((ord(char) - start_position - shift) % 26 + start_position) # сдвигаем символ к исходному и добавляем в вывод
+        else:
+            plaintext += char
     return plaintext
