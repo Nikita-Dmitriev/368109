c3e3aad9f2acf711ecd1ce4f2585b81626d97d13
Nikita-Dmitriev <113498539+Nikita-Dmitriev@users.noreply.github.com>
2024-04-03 11:29:02 +0300

    Update caesar.py

--- src/lab2/caesar.py
+++ src/lab2/caesar.py
@@ -1,6 +1,7 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -12,20 +13,20 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     for char in plaintext:
-    if char.isalpha():  # идем дальше только если символ является буквой
-        if char.isupper():  # условие, чтобы в дальнейшем не выпасть из заглавных или строчных букв
-            start_position = ord('A')
+        if char.isalpha():  # идем дальше только если символ является буквой
+            if char.isupper():  # условие, чтобы в дальнейшем не выпасть из заглавных или строчных букв
+                start_position = ord('A')
+            else:
+                start_position = ord('a')
+            ciphertext += chr((ord(char) + shift - start_position) % 26 + start_position) # сдвигаем символ на указаный shift и добавляем в вывод
         else:
-            start_position = ord('a')
-        ciphertext += chr((ord(char) + shift - start_position) % 26 + start_position) # сдвигаем символ на указаный shift и добавляем в вывод
-    else:
-        ciphertext += char  # если символ не буква, оставляем как есть
+            ciphertext += char  # если символ не буква, оставляем как есть
     return ciphertext
-
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -36,13 +37,13 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    for char in plaintext:
+    for char in ciphertext:
         if char.isalpha():
             if char.isupper():  # проверка на базу шифрования
                 start_position = ord('A')
             else:
                 start_position = ord('a')
-            plaintext += chr((ord(char) - start_position - shift) % 26 + start_position # сдвигаем символ к исходному и добавляем в вывод
+            plaintext += chr((ord(char) - start_position - shift) % 26 + start_position) # сдвигаем символ к исходному и добавляем в вывод
         else:
             plaintext += char
     return plaintext
